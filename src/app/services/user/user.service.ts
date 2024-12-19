83e0481ebae96c3ef2d4d0f46a798bc8ebd6e342
Vinicius Azambuja <vinizambs@gmail.com>
2024-12-19 12:48:41 -0300

    feat - Add routes and route guard

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 import { Observable } from 'rxjs';
 import { AuthRequest } from 'src/app/models/interfaces/user/auth/AuthRequest';
 import { AuthResponse } from 'src/app/models/interfaces/user/auth/AuthResponse';
@@ -12,7 +13,7 @@ import { environment } from 'src/environments/environment';
 })
 export class UserService {
 private API_URL = environment.API_URL;
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private cookie : CookieService) { }
 
   signupUser(requestDatas: SignupUserRequest) : Observable<SignupUserResponse> {
     return this.http.post<SignupUserResponse>(
@@ -22,5 +23,12 @@ private API_URL = environment.API_URL;
 
   authUser(requestDatas: AuthRequest) : Observable<AuthResponse> {
     return this.http.post<AuthResponse>(`${this.API_URL}/auth`, requestDatas);
+  }
+
+  isLoggedIn(): boolean {
+    //verificar se o usuario possui o token ou cookie
+    const JWT_TOKEN = this.cookie.get('USER_INFO');
+
+    return JWT_TOKEN ? true : false;
   }
 }
